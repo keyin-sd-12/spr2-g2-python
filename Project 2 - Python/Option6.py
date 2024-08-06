c24f4a8a40e87a480f01d955ca8284e09ec12e46
melanie <114537207+todayisthorsday@users.noreply.github.com>
2024-08-06 11:37:20 -0230

    Update Option6.py

--- Project 2 - Python/Option6.py
+++ Project 2 - Python/Option6.py
@@ -6,6 +6,148 @@
 
 #definitions:
 
+# Description: Print Company Profit Listing for HAB Taxi Services Company Services System
+# Author: Melanie Adams
+# Date(s): Aug 05 24
+
+
+# Define required libraries.
+from datetime import datetime
+import FormatValues as FV
+
+
+
+# Define program constants.
+
+
+
+# Define program functions.
+
+
+
+# Main report processing starts here.
+
+
+# Generate report headings.
+
+print()
+print(f" " * 23 + f"-" * 40) 
+print(f" " * 35 + f"HAB TAXI SERVICES") 
+print(f" " * 36 + f"123 ANY STREET") 
+print(f" " * 32 + f"ST. JOHN'S, NL A1A 1A1") 
+print(f" " * 36 + f"(709) 555-1234") 
+print(f" " * 23 + f"-" * 40) 
+print(f" " * 33 + f"PROFIT LISTING REPORT") 
+print( )
+print(f" " * 24 + f"From: YYYY-MM-DD" + f" " * 8 + f"To: YYYY-MM-DD" )
+print(f" " * 23 + f"-" * 40) 
+print(f" " * 23 + f"Total Revenues:" + f" " * 15 + f"$##,###.##")
+print(f" " * 23 + f"Total Expenses:" + f" " * 15 + f"$##,###.##")
+print(f" " * 23 + f"-" * 40) 
+print(f" " * 23 + f"Profit  (Loss):" + f" " * 15 + f"$##,###.##")
+print(f" " * 23 + f"-" * 40) 
+print(f" " * 31 + f"Thank you! Have a nice day!") 
+print(f" " * 23 + f"-" * 40) 
+print()
+print()
+
+
+  # print(f" " * 39 + "[ CUSTOMER INFO ]" + " " * 39)
+  # print()
+  # print(f"Policy #: {polNum:<73} Date: {invDate}")
+  # print()
+  # print()
+  # print(f"{fullName:<75} {custAdd:>24}")
+  # print(f"{custPhone:<75} {cityProvPCDSP:>24}")
+  # print()
+  # print()
+  # print(f" " * 34 + "[ POLICY COVERAGE DETAILS ]" + " " * 34)
+  # print()
+  # print(f"   # of" + " " * 20 + "Extra" + " " * 25 + "Glass" + " " * 25 + "Loaner Car")
+  # print(f"   Cars" + " " * 20 + "Liability" + " " * 21 + "Coverage" + " " * 22 + "Coverage") 
+  # print()
+  # print(f"-" * 100)
+  # print()
+  # print(f"     {numCarsInsDSP}" + " " * 25 + f"{extraLiability}" + " " * 29 + f"{optGlassCov}" + " " * 29 + f"{optLoanerCar}")
+  # print()
+  # print(f"-" * 100)
+  # print()
+  # print()
+  # print(f" " * 38 + "[ PAYMENT DETAILS ]" + " " * 34)
+  # print()
+  # print(f" Premiums for {numCarsInsDSP} cars:" + " " * 69 + f"{insPremDSP:>9s}")
+  # print(f" Extra coverages: " + " " * 72 + f"{totalExtraCostDSP:>9s}")
+  # print()
+  # print(f"-" * 25 + " " * 64 + "-" * 11)
+  # print()
+  # print(f" Subtotal: " + " " * 79 +  f"{subtotalDSP:>9s}")
+  # print(f" HST: " + " " * 84 +  f"{HSTAmtDSP:>9s}")  
+  # print()
+  # print(f"-" * 25 + " " * 64 + "-" * 11)
+  # print()
+  # print(f" Total insurance amount:" + " " * 66 +  f"{totalAMTDSP:>9s}")  
+  # print()
+  # print(f" Payment option:" + " " * 82 +  f"{paymentTypeDSP}")
+  # print(f" Down payment:" + " " * 75 +  f"-{downValueDSP:>8s}")
+  # print()
+  # print(f"-" * 25 + " " * 64 + "-" * 11)
+  # print()
+  # print(f" Total owing:" + " " * 77 +  f"{totalAMTDSP:>9s}")
+  # print(f" Processing fee (if monthly):" + " " * 61 +  f"{monthProcessingFeeDSP:>9s}")
+  # print()
+  # print()
+  # print(f" " * 27 + f"First payment of {monthPayAMTDSP} due on {monthPayDateDSP}")
+  # print()
+  # print(f"-" * 100)
+  # print()
+  # print()
+  # print(f" " * 39 + "[ PREVIOUS CLAIMS ]" + " " * 34)  
+  # print()
+  # print()
+  # print(f" Claim #" + " " * 35 + "Claim Date" + " " * 39 + "Amount")
+  # print()
+  # print(f"-" * 100)
+  # print()
+  # for item in claims:
+  #   print(f" {item[0]}" + " " * 37 + f"{item[1]}" + " " * 37 + f"{item[2]}")
+  # print()
+  # print(f"-" * 100)
+  # print()
+  # print(f" " * 27 + f"Thank you for choosing One Stop Insurance!")
+  # print()
+
+
+
+
+
+
+
+# Initialize counters and accumulators.
+
+# Open the data file.
+
+# Process each line (record) in the file in a loop.
+#for Record in f:
+   
+ 
+    # Read the record.  Grab values from the list.
+
+
+    # Perform required calculations.
+
+
+    # Display the detail line.
+
+    
+    # Update counters and accumulators.
+
+
+
+# Close the file.
+
+# Print summary data - counters and accumulators.
+
+
 def main():
     print("This is option Six") #testing main menu
 
